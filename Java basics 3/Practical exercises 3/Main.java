f6dec8e23e160c89f8d5026db5678e641a10fa49
Leonardo Cavalcanti <65732514+leohcavalcanti@users.noreply.github.com>
2022-12-18 22:17:38 -0300

    Create Main.java

---
+++ Java basics 3/Practical exercises 3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Primeiro p = new Primeiro();
+        Segundo s = new Segundo();
+        Terceiro t = new Terceiro();
+        Quarto q = new Quarto();
+        Quinto qto = new Quinto();
+    }
+}
